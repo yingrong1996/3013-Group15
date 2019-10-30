179770346b9ca0f8852c5faa9efad704f5dfbc21
Ong Eu Jin, Justin <35714621+Justin-Ong@users.noreply.github.com>
2019-10-30 10:53:11 +0800

    Update app.py

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,51 +1,34 @@
-# app.py
-from flask import Flask, request, jsonify
+from flask import Flask
+
+from __init__ import db, login_manager
+from views import view
+
 app = Flask(__name__)
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
+# Routing
+app.register_blueprint(view)
 
-    # For debugging
-    print(f"got name {name}")
 
-    response = {}
+# Config
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{username}:{password}@{host}:{port}/{database}'\
+    .format(
+        username='postgres',
+        password='justin',
+        host='localhost',
+        port=5432,
+        database='postgres'
+    )
+print(app.config)
+app.config['SECRET_KEY'] = 'A random key to use CRF for forms'
 
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+# Initialize other components
+db.init_app(app)
+login_manager.init_app(app)
 
-    # Return the response in json format
-    return jsonify(response)
 
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
-
-# A welcome message to test our server
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
-
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+if __name__ == "__main__":
+    app.run(
+        debug=True,
+        host='localhost',
+        port=5000
+    )
