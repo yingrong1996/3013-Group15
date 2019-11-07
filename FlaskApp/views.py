9cef516d05e9aead2ed345e738069a75292c8374
Justin-Ong <justinong32@gmail.com>
2019-11-07 12:42:45 +0800

    no message

--- FlaskApp/views.py
+++ FlaskApp/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, render_template, url_for
+from flask import Blueprint, redirect, render_template, url_for, request
 from flask_login import current_user, login_required, login_user
 
 from FlaskApp.__init__ import db, login_manager
