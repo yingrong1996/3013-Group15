fa0d7690253f114b870146ff10306eb4d7ff017e
khairulhazril <khairulhazril@gmail.com>
2019-11-09 19:05:45 +0800

    no message

--- FlaskApp/views.py
+++ FlaskApp/views.py
@@ -470,11 +470,11 @@ def initialize():
 
     query = """CREATE OR REPLACE FUNCTION insert_students()
         RETURNS TRIGGER AS $$ BEGIN
-        IF ((SELECT COUNT(*) FROM takes WHERE module_code = NEW.module_code)) 
+        IF ((SELECT COUNT(*) FROM takes WHERE module_code = NEW.module_code) < (SELECT quota FROM modules WHERE module_code = NEW.module_code)) 
         THEN
-            Return NEW;
-        ELSE
             INSERT INTO registration(student_id, module_code) VALUES (NEW.student_id, NEW.module_code);
+        ELSE
+            Return NEW;
         END IF;
         RETURN NULL;
         END;
